db0086c1fa7ff95c1603a13e5b505efad22c89ed
Py <166272311+Pyth0n1zed@users.noreply.github.com>
2025-11-24 21:07:37 -0600

    Update script.lua

--- script.lua
+++ script.lua
@@ -177,8 +177,8 @@ tabselector.ScrollBarThickness = 2
 tabselector.ScrollBarImageColor3 = Color3.fromRGB(102,102,102)
 
 tabselectorull:GetPropertyChangedSignal("AbsoluteContentSize"):Connect(function()
-	if tabselector.CanvasSize.Y.Offset < (tabselectorull.AbsoluteContentSize.Y) then
-		tabselector.CanvasSize = UDim2.new(0, 0, 0, tabselectorull.AbsoluteContentSize.Y + 10)
+	if tabselector.CanvasSize.Y.Offset ~= (tabselectorull.AbsoluteContentSize.Y) then
+		tabselector.CanvasSize = UDim2.new(0, 0, 0, tabselectorull.AbsoluteContentSize.Y)
 	end
 end)
 
@@ -235,8 +235,8 @@ function gui:CreateTab(name, order)
 	tabull.HorizontalAlignment = Enum.HorizontalAlignment.Left
 	tabull.VerticalAlignment = Enum.VerticalAlignment.Top
 	tabull:GetPropertyChangedSignal("AbsoluteContentSize"):Connect(function()
-		if tab.CanvasSize.Y.Offset < (tabull.AbsoluteContentSize.Y) then
-			tab.CanvasSize = UDim2.new(0, 0, 0, tabull.AbsoluteContentSize.Y + 10)
+		if tab.CanvasSize.Y.Offset ~= (tabull.AbsoluteContentSize.Y) then
+			tab.CanvasSize = UDim2.new(0, 0, 0, tabull.AbsoluteContentSize.Y)
 		end
 	end)
 
